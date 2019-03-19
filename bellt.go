3e75dc2486e7f54b5e33aa2aa9c387fb51aaf3d3
GuilhermeCaruso <guilherme.caruso@owldocs.com>
2019-03-19 09:05:12 -0300

    Remove var HTTPStatusCode

--- bellt.go
+++ bellt.go
@@ -24,60 +24,6 @@ var (
 		"DELETE",
 	}
 	mainRouter *Router
-
-	// HTTPStatusCode verbose status code used for quick debugging Http.
-	HTTPStatusCode = map[string]int{
-
-		"CONTINUE":                        100,
-		"SWITCHING_PROTOCOLS":             101,
-		"OK":                              200,
-		"CREATED":                         201,
-		"ACCEPTED":                        202,
-		"NON_AUTHORITATIVE_INFORMATION":   203,
-		"NO_CONTENT":                      204,
-		"RESET_CONTENT":                   205,
-		"PARTIAL_CONTENT":                 206,
-		"MULTIPLE_CHOICES":                300,
-		"AMBIGUOUS":                       300,
-		"MOVED_PERMANENTLY":               301,
-		"MOVED":                           301,
-		"FOUND":                           302,
-		"REDIRECT":                        302,
-		"SEE_OTHER":                       303,
-		"REDIRECT_METHOD":                 303,
-		"NOT_MODIFIED":                    304,
-		"USE_PROXY":                       305,
-		"UNUSED":                          306,
-		"TEMPORARY_REDIRECT":              307,
-		"REDIRECT_KEEP_VERB":              307,
-		"BAD_REQUEST":                     400,
-		"UNAUTHORIZED":                    401,
-		"PAYMENT_REQUIRED":                402,
-		"FORBIDDEN":                       403,
-		"NOT_FOUND":                       404,
-		"METHOD_NOT_ALLOWED":              405,
-		"NOT_ACCEPTABLE":                  406,
-		"PROXY_AUTHENTICATION_REQUIRED":   407,
-		"REQUEST_TIMEOUT":                 408,
-		"CONFLICT":                        409,
-		"GONE":                            410,
-		"LENGTH_REQUIRED":                 411,
-		"PRECONDITION_FAILED":             412,
-		"REQUEST_ENTITY_TOOLARGE":         413,
-		"REQUEST_URI_TOO_LONG":            414,
-		"UNSUPPORTED_MEDIA_TYPE":          415,
-		"REQUESTED_RANGE_NOT_SATISFIABLE": 416,
-		"EXPECTATION_FAILED":              417,
-		"UNPROCESSABLE_ENTITY":            422,
-		"UPGRADE_REQUIRED":                426,
-		"INTERNAL_SERVER_ERROR":           500,
-		"NOT_IMPLEMENTED":                 501,
-		"BAD_GATEWAY":                     502,
-		"SERVICE_UNAVAILABLE":             503,
-		"GATEWAY_TIMEOUT":                 504,
-		"HTTP_VERSION_NOT_SUPPORTED":      505,
-		"NETWORK_CONNECT_TIMEOUT":         599,
-	}
 )
 
 // Router is a struct responsible for storing routes already available (Route)
@@ -187,7 +133,7 @@ func redirectBuiltRoute(w http.ResponseWriter, r *http.Request) {
 			allParams).ServeHTTP(w, r)
 	} else {
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(HTTPStatusCode["NOT_FOUND"])
+		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(`{"msg": "route not found"}`))
 	}
 
@@ -418,7 +364,7 @@ func gateMethod(next http.HandlerFunc, methods ...string) http.HandlerFunc {
 			}
 		}
 
-		w.WriteHeader(HTTPStatusCode["NOT_FOUND"])
+		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(`{"error": "The method for this route doesnt exist"}`))
 
 	}
@@ -507,7 +453,7 @@ func (pr *ParamReceiver) GetVar(variable string) interface{} {
 // Function used in application health routing.
 func healthApplication(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(HTTPStatusCode["OK"])
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"alive": "Server running"}`))
 }
 
